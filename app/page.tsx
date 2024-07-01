e38ec0df6c93f0c64b224ad5612faaf45aae2b06
cadzchua <cadidic3h@gmail.com>
2024-07-01 15:36:27 +0800

    feat: floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import Grid from "@/components/Grid";
 import Main from "@/components/Main";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Image from "next/image";
+import { TiHome } from "react-icons/ti";
 export default function Home() {
   return (
     <main
@@ -8,7 +11,11 @@ export default function Home() {
     mx-auto sm:px-10 px-5"
     >
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <TiHome /> }]}
+        />
         <Main />
+        <Grid />
       </div>
     </main>
   );
