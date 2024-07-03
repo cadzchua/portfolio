f97bde09a5d817f42f8dfecb3672b42c7e007e1a
cadzchua <cadidic3h@gmail.com>
2024-07-03 20:00:29 +0800

    feat: sizing of aboutme text

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -14,10 +14,10 @@ const Aboutme = () => {
           className="object-contain rounded-[22px] mx-auto"
         ></img>
 
-        <p className="md:text-3xl text-2xl text-center text-black mt-6 mb-4 dark:text-neutral-200">
+        <p className="sm:text-lg text lg:text-2xl text-base text-center text-black mt-6 mb-7 dark:text-neutral-200">
           About Me
         </p>
-        <p className="sm:text-md md:text-lg text-sm text-justify text-neutral-600 dark:text-neutral-400">
+        <p className="sm:text-sm lg:text-lg text-xs text-justify sm:mx-8 mx-5  text-neutral-600 dark:text-neutral-400">
         &emsp; I am a freshman student at Nanyang Technological University, 
         I am passionate about software development and AI/ML. 
         I like to learn new things everyday and I also participate in CTFs. 
