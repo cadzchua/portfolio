b430f43a7a949977ab6b1a64b71cc9e0f6eecf46
cadzchua <cadidic3h@gmail.com>
2024-07-08 13:49:27 +0800

    chore: didnt want to link all via link svg soo i changed languages only

--- data/Aboutme.tsx
+++ data/Aboutme.tsx
@@ -1,145 +1,154 @@
 export const categories = [
   {
-    name: 'Frontend',
+    name: "Frontend",
     items: [
       {
         id: 1,
         name: "React",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-    
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
       },
       {
         id: 2,
         name: "Vite.js",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
       },
       {
         id: 3,
         name: "Next.js",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
       },
       {
         id: 4,
         name: "Tailwind",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
       },
       {
         id: 5,
         name: "Chakra UI",
-        image: "/chakraui.png"
+        image: "/chakraui.png",
       },
       {
         id: 6,
         name: "Aceternity UI",
-        image: "/aceternityui.png"
+        image: "/aceternityui.png",
       },
       {
         id: 7,
         name: "Streamlit",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/streamlit/streamlit-original.svg"
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/streamlit/streamlit-original.svg",
       },
     ],
   },
   {
-    name: 'Backend',
+    name: "Backend",
     items: [
       {
         id: 1,
         name: "Flask",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg"
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg",
       },
       {
         id: 2,
         name: "PostgreSQL",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
       },
       {
         id: 3,
         name: "Docker",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg"
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
       },
     ],
   },
   {
-    name: 'AI/ML',
+    name: "AI/ML",
     items: [
       {
         id: 1,
         name: "Pytorch",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg"
-    
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg",
       },
       {
         id: 2,
         name: "Scikitlearn",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg"
-    
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg",
       },
       {
         id: 3,
         name: "Pandas",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original.svg"
-    
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original.svg",
       },
       {
         id: 4,
         name: "Tensorflow",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg"
-    
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg",
       },
       {
         id: 5,
         name: "Keras",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/keras/keras-original.svg"
-    
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/keras/keras-original.svg",
       },
       {
         id: 6,
         name: "HuggingFace",
-        image: "/huggingface.png"
-    
+        image: "/huggingface.png",
       },
     ],
   },
   {
-    name: 'Languages',
+    name: "Languages",
     items: [
       {
         id: 1,
         name: "Python",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
-    
+        image: "/python_language.png",
       },
       {
         id: 2,
         name: "C",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"
+        image: "/c_language.svg",
       },
       {
         id: 3,
         name: "JavaScript",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        image:
+          "/javascript_language.svg",
       },
       {
         id: 4,
         name: "TypeScript",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+        image:
+          "/typescript_language.svg",
       },
       {
         id: 5,
         name: "HTML",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+        image:
+          "/html_language.svg",
       },
       {
         id: 6,
         name: "CSS",
-        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+        image:
+          "/css_language.svg",
       },
       {
         id: 7,
         name: "Verilog",
-        image: "/verilog_language.png"
+        image: "/verilog_language.png",
       },
     ],
   },
-]
+];
